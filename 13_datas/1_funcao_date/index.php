4c127fef2831087b02b7bf3fe25d6ea139ad251e
Erick <erick1junior@hotmail.com>
2022-04-12 06:50:37 -0300

    Add Date function

---
+++ 13_datas/1_funcao_date/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    $d = date('d/m/y'); //date/ month/ year
+    echo $d . "<br>";
+    // 12/04/22
+
+    $d2 = date('d, M Y');
+    echo $d2 . "<br>";
+    // 12, Apr 2022
+
+    $d3 = date('d/m/Y');
+    echo $d3 . "<br>";
+    // 12/04/2022
